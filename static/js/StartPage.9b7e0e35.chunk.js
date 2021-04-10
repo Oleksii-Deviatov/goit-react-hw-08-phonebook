95a92693cba044d9466cdfa538526a80668c4e49
Alexeii Deviatov <lewyk@hotmail.com>
2021-04-10 14:57:12 +0300

    Updates

---
+++ static/js/StartPage.9b7e0e35.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[4],{176:function(e,t,o){"use strict";o.r(t),o.d(t,"default",(function(){return r}));var n=o.p+"static/media/book.8d41de93.png",i=o(2);var r=function(){return Object(i.jsxs)(i.Fragment,{children:[Object(i.jsx)("img",{src:n,alt:"book immage"}),Object(i.jsx)("p",{children:"Please Login or Register"})]})}}}]);
+//# sourceMappingURL=StartPage.9b7e0e35.chunk.js.map
